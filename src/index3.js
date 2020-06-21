ed532e7adfc09f5689168514a8eb45d147413279
Diana Garbuz <GDI.1999@yandex.ru>
2020-06-21 14:56:49 +0300

    добавила несколько методов для блока результатов

--- src/index3.js
+++ src/index3.js
@@ -1,6 +1,5 @@
 import NewsApi from './js/api/NewsApi';
 import NewsCardList from './js/components/NewsCardList';
-import { calcTimeForRequest } from './js/utils/utils';
 
 const apiKey = 'eb14994f064f4a27bfe88e8a815ccd48';
 const newsApi = new NewsApi({
@@ -9,16 +8,6 @@ const newsApi = new NewsApi({
 });
 
 (function () {
-  const container = document.querySelector('.result__container');
-  const search = document.querySelector('.search__input');
-  const searchButton = document.querySelector('.search__button');
   // this.createCard = createCard;
-  const newsCardList = new NewsCardList(newsApi, container, search, searchButton);
+  const newsCardList = new NewsCardList(newsApi);
 }());
-newsApi.getNews('короновирус', calcTimeForRequest(), 2)
-  .then((date) => {
-    console.log(date);
-  })
-  .catch((err) => {
-    console.log(err);
-  });
--- src/js/components/NewsCardList.js
+++ src/js/components/NewsCardList.js
@@ -1,24 +1,61 @@
+import { calcTimeForRequest } from '../utils/utils';
+
 export default class NewsCardList {
-  constructor(api, container, search, searchButton) {
+  constructor(api) {
     this.api = api;
-    this.container = container;
-    this.search = search;
-    this.searchButton = searchButton;
+    this.resultBlock = document.querySelector('.result');
+    this.container = document.querySelector('.result__container');
+    this.search = document.querySelector('.search__input');
+    this.searchButton = document.querySelector('.search__button');
+    this.nothingFoundBlock = document.querySelector('.waiting-error');
+    this.preloader = document.querySelector('.waiting');
+    this.serverError = document.querySelector('.server-error');
     this._setHandlers();
+    this._initialState();
   }
 
   _setHandlers() {
     this.searchButton.addEventListener('click', this._validateSearch.bind(this));
   }
 
+  _initialState() { // скрывает все блоки
+    this.resultBlock.classList.add('hiddenElement');
+    this.nothingFoundBlock.classList.add('hiddenElement');
+    this.preloader.classList.add('hiddenElement');
+    this.serverError.classList.add('hiddenElement');
+  }
 
   _validateSearch() {
     this.search.placeholder = 'Введите тему новости';
     if (!this.search.checkValidity()) {
       this.search.placeholder = 'Нужно ввести ключевое слово';
     } else {
-      console.log(this.search.value);
+      this.searchNews(this.search.value); // отправляем запрос NewsApi
     }
-    // иначе запрос к api
+  }
+
+  searchNews(keyWord) {
+    this._initialState(); // скрываю все блоки
+    this.preloader.classList.remove('hiddenElement'); // включаю прелоудер
+    this.api.getNews(keyWord, calcTimeForRequest(), 2)
+      .then((date) => {
+        this.preloader.classList.add('hiddenElement');
+        if (date.articles.length === 0) { // если нет новостей, показываю блок "ничего не найдено"
+          this.nothingFoundBlock.classList.remove('hiddenElement');
+        } else {
+          this.resultBlock.classList.remove('hiddenElement');
+          // вызвать добавление карточек
+          console.log(date);
+        }
+      })
+      .catch((err) => {
+        this.preloader.classList.add('hiddenElement');
+        this.serverError.classList.remove('hiddenElement'); // ошибка сервера
+        console.log(err);
+      });
   }
 }
+/* Когда данные получены, в блоке результатов исчезает прелоудер, и появляются карточки. Их следует расположить в линию по 3 на каждой строке на разрешении 1280 пикселей. Если сжимать окно браузера, карточки переносятся на следующую строку. */
+/* Если карточек больше трёх, отрисовываются только 3, а под ними появляется кнопка «Показать ещё». По нажатию отрисовываются ещё три, а кнопка сдвигается ниже, чтобы оказаться внизу блока с карточками.
+Когда все карточки отрисованы, кнопка «Показать ещё» должна пропасть.
+Разберём подробнее устройство отдельной карточки в блоке с результатами. */
