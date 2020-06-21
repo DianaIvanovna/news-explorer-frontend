bd64b9e85b41bddde63003aeaeebd1f0ba49e983
Diana Garbuz <GDI.1999@yandex.ru>
2020-06-21 18:00:55 +0300

    отрисовка карточек по 3 штуки

--- src/js/components/NewsCardList.js
+++ src/js/components/NewsCardList.js
@@ -10,7 +10,9 @@ export default class NewsCardList {
     this.nothingFoundBlock = document.querySelector('.waiting-error');
     this.preloader = document.querySelector('.waiting');
     this.serverError = document.querySelector('.server-error');
+    this.resultButton = document.querySelector('.result__button');
     this.createCard = createCard;
+    this.articles = undefined; // массив статей
     this._setHandlers();
     this._initialState();
     this.addCard.bind(this);
@@ -18,6 +20,7 @@ export default class NewsCardList {
 
   _setHandlers() {
     this.searchButton.addEventListener('click', this._validateSearch.bind(this));
+    this.resultButton.addEventListener('click', this.addCard.bind(this));
   }
 
   _initialState() { // скрывает все блоки
@@ -25,6 +28,7 @@ export default class NewsCardList {
     this.nothingFoundBlock.classList.add('hiddenElement');
     this.preloader.classList.add('hiddenElement');
     this.serverError.classList.add('hiddenElement');
+    this.resultButton.classList.remove('hiddenElement');
   }
 
   _validateSearch() {
@@ -39,15 +43,15 @@ export default class NewsCardList {
   searchNews(keyWord) {
     this._initialState(); // скрываю все блоки
     this.preloader.classList.remove('hiddenElement'); // включаю прелоудер
-    this.api.getNews(keyWord, calcTimeForRequest(), 4)
+    this.api.getNews(keyWord, calcTimeForRequest(), 20)
       .then((date) => {
         this.preloader.classList.add('hiddenElement');
         if (date.articles.length === 0) { // если нет новостей, показываю блок "ничего не найдено"
           this.nothingFoundBlock.classList.remove('hiddenElement');
         } else {
           this.resultBlock.classList.remove('hiddenElement');
-          this.addCard(date.articles);
-          // вызвать добавление карточек
+          this.articles = date.articles;
+          this.addCard(); // вызвать добавление карточек
           console.log(date);
         }
       })
@@ -58,11 +62,21 @@ export default class NewsCardList {
       });
   }
 
-  addCard(articles) {
-    articles.forEach((article) => {
-      const news = this.createCard(this.api, article);
-      const cardElement = news.create();
-      this.container.append(cardElement); // новые карточки вставляются вперед
-    });
+  addCard() {
+    if (this.articles.length > 3) {
+      for (let i = 0; i < 3; i += 1) {
+        const news = this.createCard(this.api, this.articles[i]);
+        const cardElement = news.create();
+        this.container.append(cardElement);
+      }
+      this.articles = this.articles.slice(3);
+    } else {
+      this.resultButton.classList.add('hiddenElement');
+      for (let i = 0; i < this.articles.length; i += 1) {
+        const news = this.createCard(this.api, this.articles[i]);
+        const cardElement = news.create();
+        this.container.append(cardElement);
+      }
+    }
   }
 }
